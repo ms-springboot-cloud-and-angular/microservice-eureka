9fa13ab134ce0d26238857879f2f40f4c542951d
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-10 13:46:20 +0200

    Enable eureka server

--- src/main/java/com/joseluisestevez/ms/app/eureka/MicroserviceEurekaApplication.java
+++ src/main/java/com/joseluisestevez/ms/app/eureka/MicroserviceEurekaApplication.java
@@ -2,7 +2,9 @@ package com.joseluisestevez.ms.app.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MicroserviceEurekaApplication {
 
