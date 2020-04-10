594f94b56a515bd0cd5a098697ba3ab71d644c0a
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-10 13:27:50 +0200

    New SpringBoot Eureka Server Project

---
+++ src/main/java/com/joseluisestevez/ms/app/eureka/MicroserviceEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.app.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joseluisestevez/ms/app/eureka/MicroserviceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
